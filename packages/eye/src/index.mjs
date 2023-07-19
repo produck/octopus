e5d87696fddaac9d80ba3a3307f34dcc69839c84
luoxiaohuio <leoword@qq.com>
2023-07-19 17:16:34 +0800

    @produck/octopus-eye: [INIT]

---
+++ packages/eye/src/index.mjs
@@ -0,0 +1,3 @@
+import * as Crank from '@produck/crank';
+
+export const Evaluator = new Crank.Engine();
---
+++ packages/eye/src/index.spec.mjs
@@ -0,0 +1,10 @@
+import * as assert from 'node:assert/strict';
+import { describe, it } from 'mocha';
+
+import * as Evaluator from './index.mjs';
+
+describe('Evaluator::', function () {
+	it('should get an evaluator.', function () {
+
+	});
+});
