9f37f46b9afcd9d1930ab4dd201c709307670ffd
luoxiaohuio <leoword@qq.com>
2023-07-19 17:16:35 +0800

    @produck/octopus-eye: [FIXED] Extern

--- packages/eye/src/index.d.ts
+++ packages/eye/src/index.d.ts
@@ -1,13 +1,33 @@
-import * as Crank from '@produck/crank'
+import * as Crank from '@produck/crank';
+import { Schema } from '@produck/mold';
 
-export class Extern extends Crank.Extern {
-	readonly args1: Array<any>;
+interface DumpOptions {
+	value: any[];
+	children: DumpOptions[];
 }
 
-interface DumpSchema {
+interface Job {
+	id: string,
+	ok: boolean,
+	error: any,
+	target?: any
+}
 
+export class Extern extends Crank.Extern {
+	readonly dump?: DumpOptions;
+	readonly creating: Array<{ id: string, craft: string, source: any }>
+
+	hasJob: (id: string) => boolean;
+	fetchJob: (id: string) => Job;
+	planJob: (id: string, craft: string, source: any) => void;
+	saveData: (key: any, value: any) => void;
+	fetchData: (key: any) => any;
+	hasData: (key: any) => boolean;
+	assertCraftAndSource: (name: string, source: any) => void;
 }
 
 export const Evaluator: ReturnType<typeof Crank.defineEngine<{
-	val: () => any
+	[key in 'val' | 'run' | 'all']: () => any;
 }>>
+
+export const DumpSchema: Schema<DumpOptions>;
